72f05812b3a990f5e59c95b133188587aeb41b72
Benjamin Mmari <benmmari92@gmail.com>
2018-01-31 10:30:38 +0200

    made slight changes to logic, updated contract and tests

--- contracts/SimpliWaterToken.sol
+++ contracts/SimpliWaterToken.sol
@@ -9,69 +9,67 @@ contract SimpliWaterToken is CustomMintableToken, BurnableToken {
     uint private normalChargePerLiter; //1000 = 1R
     mapping(address => Meter) private registeredMeters;
     uint private constant ETHERIUM_TO_SWT_EXCHANGE_RATE = 2; // 1 ETH = 2 SWT for the sake of the proof of concept.
+    mapping(address => uint) private meterUsage;
 
     struct Meter {
         address meterAddress;
-        mapping(address => HouseMember) houseMembers;
+        mapping(uint => HouseMember) houseMembers;
         uint totalHouseMembers;
+        uint registrationEpochTime;
     }
 
     struct HouseMember {
         bytes16 firstName;
         bytes32 lastName;
-        address memberAddress;
+        uint memberID; //form of unique identification like ID or passport
     }
 
     event MeterRegisteredEvent(address _meterAddress);
-    event UserAddedEvent(address _meterAddress, address _memberAddress);
+    event UserAddedEvent(address _meterAddress, uint _memberID);
     
 
     function SimpliWaterToken() public {
         totalSupply_ = 0; // New coins will continuously be minted when needed.
-        dailyLimit = 50000;
-        penaltyChargePerLiter = 4000;
+        //making this variable will make things more complex when it comes to penalty payments
+        //so will keep these constant for the proof of concept.
+        dailyLimit = 50000; 
+        penaltyChargePerLiter = 4000; 
         normalChargePerLiter = 1000;
     }
    
-    function registerMeter(address _meterAddress) onlyOwner public {
+    function registerMeter(address _meterAddress, uint _registrationEpochTime) onlyOwner public {
         require(registeredMeters[_meterAddress].meterAddress == address(0));
-        var newMeter = Meter({meterAddress: _meterAddress, totalHouseMembers:0});
+        var newMeter = Meter({meterAddress: _meterAddress, totalHouseMembers:0, registrationEpochTime: _registrationEpochTime});
         registeredMeters[_meterAddress] = newMeter;
+        meterUsage[_meterAddress] = 0;
+        
         MeterRegisteredEvent(_meterAddress);
     }
 
-    function retrieveMeterHouseMemberTotal(address _meterAddress) onlyOwner view public returns(address meterAddress, uint totalHouseMembers) {
+    function retrieveMeterStats(address _meterAddress) view public returns(address meterAddress, uint balance, uint totalHouseMembers, uint registrationEpochTime, uint usage) {
         require(registeredMeters[_meterAddress].meterAddress != address(0));
-        return (registeredMeters[_meterAddress].meterAddress, registeredMeters[_meterAddress].totalHouseMembers);
+        return (registeredMeters[_meterAddress].meterAddress, registeredMeters[_meterAddress].totalHouseMembers, registeredMeters[_meterAddress].registrationEpochTime, balanceOf(_meterAddress), getMeterUsage(_meterAddress));
     }
 
-   function addUserToMeter(address _meterAddress, bytes16 _firstName, bytes16 _lastName, address _memberAddress) onlyOwner public {
+    function retrieveUtilityConstants() view public returns(uint _normalCharge, uint penaltyCharge, uint _dailyLimit) {
+        return (normalChargePerLiter, penaltyChargePerLiter, dailyLimit);
+    }
+
+   function addUserToMeter(address _meterAddress, bytes16 _firstName, bytes16 _lastName, uint _memberID) onlyOwner public {
         require(registeredMeters[_meterAddress].meterAddress != address(0));
-        require(registeredMeters[_meterAddress].houseMembers[_memberAddress].memberAddress == address(0));        
-        var newMember = HouseMember(_firstName, _lastName, _memberAddress);
-        registeredMeters[_meterAddress].houseMembers[_memberAddress] = newMember;
+        require(registeredMeters[_meterAddress].houseMembers[_memberID].memberID == 0);        
+        var newMember = HouseMember(_firstName, _lastName, _memberID);
+        registeredMeters[_meterAddress].houseMembers[_memberID] = newMember;
         registeredMeters[_meterAddress].totalHouseMembers += 1;
-        UserAddedEvent(_meterAddress, _memberAddress);
-    }
-
-    function setDailyLimit(uint _dailyLimit) onlyOwner public {
-        dailyLimit = _dailyLimit;
+        UserAddedEvent(_meterAddress, _memberID);
     }
 
    function getDailyLimit() view public returns(uint _dailyLimit) {
         return dailyLimit;
     }
 
-    function setPenaltyChargePerLiter(uint _penaltyCharge) onlyOwner public {
-        penaltyChargePerLiter = _penaltyCharge;
-    }
-
    function getPenaltyChargePerLiter() view public returns (uint penaltyCharge) {
         return penaltyChargePerLiter;
-    }
-
-    function setNormalChargePerLiter(uint _normalCharge) onlyOwner public {
-        normalChargePerLiter = _normalCharge;
     }
 
     function getNormalChargePerLiter() view public returns(uint _normalCharge) {
@@ -91,6 +89,7 @@ contract SimpliWaterToken is CustomMintableToken, BurnableToken {
         require(registeredMeters[msg.sender].meterAddress != address(0));
         require(balances[msg.sender] >= _tokensToBurn);
         super.burn(_tokensToBurn);
+        meterUsage[msg.sender] += _tokensToBurn;        
     }
 
     function topUpBalanceWithEther(address _meterAddress, uint _value) payable public {
@@ -98,12 +97,12 @@ contract SimpliWaterToken is CustomMintableToken, BurnableToken {
     }
 
 
-    function getTokenBalance(address _meterAddress) view public returns(uint) {
-    return balances[_meterAddress];
+    function getMeterUsage(address _meterAddress) view public returns(uint) {
+        return meterUsage[_meterAddress];
     }
 
     function getEthBalance(address _meterAddress) view public returns(uint) {
-    return _meterAddress.balance;
+        return _meterAddress.balance;
     }
 
    }
--- test/simpliwatertoken-tests.js
+++ test/simpliwatertoken-tests.js
@@ -4,9 +4,7 @@ contract('SimpliWaterToken', function (accounts) {
    let contract;
    const admin = accounts[0];
    const meter = accounts[1];      
-   const user_1 = accounts[2];      
-   const user_2 = accounts[3];      
-   const user_3 = accounts[4];   
+   const user_1 = accounts[2];       
 
    //TODO: Implement test that cause exceptions.
 
@@ -19,42 +17,29 @@ contract('SimpliWaterToken', function (accounts) {
         assert.equal(returnedTotalSupply, 0);
       });
 
-      it("Owner can set daily limit", async function() {
-        await contract.setDailyLimit(87000);
-        let returnedDailyLimit = await contract.getDailyLimit();
-        assert.equal(returnedDailyLimit, 87000);
-      });
-
-
-      it("Owner can set penalty charge", async function() {
-        await contract.setPenaltyChargePerLiter(100);
-        let returnedPenaltyChargePerLiter = await contract.getPenaltyChargePerLiter();
-        assert.equal(returnedPenaltyChargePerLiter, 100);
-      });
-
-
-      it("Owner can set normal charge", async function() {
-        await contract.setNormalChargePerLiter(300);
-        let returnedPenaltyChargePerLiter = await contract.getNormalChargePerLiter();
-        assert.equal(returnedPenaltyChargePerLiter, 300);
-      });
 
       it("Owner can register meter with 0 users", async function() {
-        await contract.registerMeter(meter);
-        let returnedMeter = await contract.retrieveMeterHouseMemberTotal(meter);
-        assert.equal(returnedMeter[0], meter);
-        assert.equal(returnedMeter[1], 0);  
+        let epochTime = 1517385490;
+        await contract.registerMeter(meter, epochTime);
+        let returnedMeterStats = await contract.retrieveMeterStats(meter);        
+        let returnedMeterUsage = await contract.getMeterUsage(meter);
+        assert.equal(returnedMeterStats[0], meter);
+        assert.equal(returnedMeterStats[1], 0);
+        assert.equal(returnedMeterStats[2], epochTime);
+        assert.equal(returnedMeterStats[3], 0);                    
+        assert.equal(returnedMeterStats[4], 0);          
+        assert.equal(returnedMeterUsage, 0);          
       });
 
       it("Owner can add user to meter", async function() {
         let testUserFirstName = "Satoshi";
         let testUserLastName = "Nakomoto";
-        let testUserAccount = user_1;
+        let testUserID = 123456789;
         
-        await contract.addUserToMeter(meter, testUserFirstName, testUserLastName, testUserAccount);
-        let returnedMeter = await contract.retrieveMeterHouseMemberTotal(meter);
-        assert.equal(returnedMeter[0], meter);
-        assert.equal(returnedMeter[1], 1);        
+        await contract.addUserToMeter(meter, testUserFirstName, testUserLastName, testUserID);
+        let returnedMeterStats = await contract.retrieveMeterStats(meter);
+        assert.equal(returnedMeterStats[0], meter);
+        assert.equal(returnedMeterStats[1], 1);        
       });
 
 
@@ -67,16 +52,37 @@ contract('SimpliWaterToken', function (accounts) {
       });
 
 
-      it("Meter can burn an arbitrary amount", async function() { 
+      it("Meter can burn an arbitrary amount and increment overall usage", async function() { 
         let burnAmount = 500;      
-        let meterBalanceBefore = await contract.getTokenBalance(meter);  
+        let meterUsageBefore = await contract.getMeterUsage(meter);          
+        let meterBalanceBefore = await contract.balanceOf(meter);  
         await contract.burn(burnAmount, {from: meter});
         let meterBalanceAfter = await contract.balanceOf(meter); 
+        let meterUsageAfter = await contract.getMeterUsage(meter);                  
         assert.equal(parseInt(meterBalanceAfter), parseInt(meterBalanceBefore) - parseInt(burnAmount));
+        assert.equal(parseInt(meterUsageBefore), parseInt(meterUsageAfter) - parseInt(burnAmount));        
       });
 
 
-      it("An arbitrary accountholder can send ether and top up the meter balance an arbitrary amount", async function() { 
+      it("An arbitrary entity can retrieve stats for a meter", async function() { 
+        let meterBalance = await contract.balanceOf(meter);  
+        let returnedMeterStats = await contract.retrieveMeterStats(meter);         
+        assert.equal(returnedMeterStats[0], meter);
+        assert.equal(parseInt(returnedMeterStats[3]), parseInt(meterBalance));
+      });
+
+      it("An arbitrary entity can retrieve utility constants", async function() { 
+        let returnedUtilityConstants = await contract.retrieveUtilityConstants(); 
+        let initialDailyLimit = 50000;
+        let initialPenaltyCharge = 4000;        
+        let initialNormalCharge = 1000;                        
+        assert.equal(returnedUtilityConstants[0], initialNormalCharge);
+        assert.equal(returnedUtilityConstants[1], initialPenaltyCharge);
+        assert.equal(returnedUtilityConstants[2], initialDailyLimit);
+      });
+
+
+      it("An arbitrary entity can send ether and top up the meter balance", async function() { 
         let topUpAmount = 5000;      
         let weiValue = 10;
         let contractBalanceBefore = await contract.getEthBalance(meter); 
